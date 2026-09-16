de712fe74bbed6a521b43d74886096b7d5702b80
Daouda ZONGO <daoudaz6666@gmail.com>
2026-09-16 21:19:00 +0000

    Ajout module library_website - page d'accueil personnalisée

---
+++ library_website/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Library Website',
+    'version': '1.0',
+    'summary': "Site web de la Bibliothèque Box Africa",
+    'category': 'Website',
+    'depends': ['website', 'library_management'],
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
